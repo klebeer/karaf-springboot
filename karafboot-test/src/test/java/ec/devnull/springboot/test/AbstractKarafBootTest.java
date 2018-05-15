fdb1165c5f28c5f1e00a3eda9272d037b8b0b76b
Kleber Ayala <kleber.ayala@gmail.com>
2018-05-15 14:21:17 -0500

    fix: test dependencies

--- karafboot-test/src/test/java/ec/devnull/springboot/test/AbstractKarafBootTest.java
+++ karafboot-test/src/test/java/ec/devnull/springboot/test/AbstractKarafBootTest.java
@@ -4,14 +4,14 @@ package ec.devnull.springboot.test;
 import org.apache.karaf.features.FeaturesService;
 import org.ops4j.pax.exam.ConfigurationManager;
 import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.options.MavenArtifactProvisionOption;
 import org.ops4j.pax.exam.options.MavenUrlReference;
 
 import javax.inject.Inject;
 import java.io.File;
 
 import static org.junit.Assert.assertTrue;
-import static org.ops4j.pax.exam.CoreOptions.composite;
-import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.CoreOptions.*;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.*;
 
 /**
@@ -38,12 +38,15 @@ public class AbstractKarafBootTest {
         return replaceConfigurationFile("etc/" + configName, new File("src/test/resources/" + configName));
     }
 
+    protected MavenArtifactProvisionOption mvnBundle(String groupId, String artifactId) {
+        return mavenBundle(groupId, artifactId).versionAsInProject();
+    }
+
     protected Option karafDefaults() {
         return composite(
-                // KarafDistributionOption.debugConfiguration("5005", true),
                 karafDistributionConfiguration().frameworkUrl(karafUrl)
                         .unpackDirectory(new File("target/exam")).useDeployFolder(false), //
-                configureConsole().ignoreLocalConsole().ignoreRemoteShell(), //
+                configureConsole().ignoreRemoteShell().ignoreLocalConsole(),
                 keepRuntimeFolder());
     }
 }
